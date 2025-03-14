cd0173f280f2570d73c7e8935857f241efacc50a
James <electionnightspecial@gmail.com>
2025-03-14 13:36:09 +0000

    Add edit mode

--- src/components/MarinersView.vue
+++ src/components/MarinersView.vue
@@ -21,6 +21,7 @@
             <th>Birth Year</th>
             <th>Death Year</th>
             <th>Place of Birth</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody class="divide-y divide-gray-200">
@@ -29,6 +30,17 @@
             <td>{{ mariner.year_of_birth || '-' }}</td>
             <td>{{ mariner.year_of_death || '-' }}</td>
             <td>{{ mariner.place_of_birth || '-' }}</td>
+            <td class="text-center">
+              <button 
+                @click="editMariner(mariner)" 
+                class="text-blue-500 hover:text-blue-700 focus:outline-none mx-auto"
+                title="Edit mariner"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                </svg>
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -111,6 +123,12 @@ export default {
     // Watch for page changes
     watch(currentPage, loadMariners);
 
+    function editMariner(mariner) {
+      console.log('Edit mariner:', mariner);
+      // You can implement the edit functionality here
+      // For example, navigate to an edit page or open a modal
+    }
+
     return {
       mariners,
       totalItems,
@@ -122,7 +140,8 @@ export default {
       prevPage,
       itemsPerPage: ITEMS_PER_PAGE,
       searchTerm,
-      handleSearch
+      handleSearch,
+      editMariner
     };
   }
 }
